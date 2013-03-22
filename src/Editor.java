ee744d59144ab4613b5fad91280ab9edaed2ba47
jmp3833 <jmp3833@rit.edu>
2013-03-22 14:49:43 -0400

    Created the main GUI window for the text editor.

---
+++ src/Editor.java
@@ -0,0 +1,15 @@
+/**
+ * @author: Justin Peterson
+ * @email: Jmp3833@rit.edu
+ * Editor.java is the main class which will invoke the User Interface
+ */
+
+import Views.MainGUI;
+
+public class Editor {
+	
+	public static void main(String args[]){
+		new MainGUI().execute();
+	}
+
+}
