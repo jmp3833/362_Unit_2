cd8c146951fd5ef36b3bb5a4f8ae9e831b1ec55e
Bogart <hakimakitak@gmail.com>
2013-03-25 06:39:46 -0400

    In TextWindow: changed JTextPane to JTextArea and added options for tab resizing and wrapping (line, letter, and word).

--- src/Views/TextWindow.java
+++ src/Views/TextWindow.java
@@ -12,11 +12,12 @@ import Views.TextTabWindow;
 import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 
-public class TextWindow extends JTextPane{
+public class TextWindow extends JTextArea{
 	
 	private static final long serialVersionUID = 1L;
 
@@ -26,10 +27,16 @@ public class TextWindow extends JTextPane{
 	 */
 	public TextWindow(String windowName, TextTabWindow tabWindow){
 		setLayout(new BorderLayout(5,10));
-		new JTextPane();
-		//setEditorKit(new HTMLEditorKit());
-		//HTMLDocument doc = new HTMLDocument();
-		//setStyledDocument(doc);
+		new JTextArea();
+		
+		//allows line wrapping; defaults to by letter if wrapStyleWord is false
+		setLineWrap(true);
+		
+		//will not work if lineWrap is false, true wraps by word
+		setWrapStyleWord(true);
+		
+		//change the tab size; default is 8
+		setTabSize(8);
 		
 		tabWindow.addTab(windowName, this);
 		setVisible(true);
